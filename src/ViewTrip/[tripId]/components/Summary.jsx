35585048df08223bb7549532d1a6453a867302f3
kirkyky123 <131261278+kirkyky123@users.noreply.github.com>
2024-09-07 09:01:29 -0400

    fixed footer color

--- src/ViewTrip/[tripId]/components/Summary.jsx
+++ src/ViewTrip/[tripId]/components/Summary.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Summary({ trip }) {
   return <div>
     {/* Include summary of trip*/}
-    <h2 className="text-gray-500 text-center">Created by Kiran Chhetri</h2>
+    <h2 className="text-white text-center">Created by Kiran Chhetri</h2>
   </div>;
 }
 
