7e035c3f3e843cc57a08c7e0f4a40c6514d2089a
kirkyky123 <131261278+kirkyky123@users.noreply.github.com>
2024-12-03 19:57:41 -0500

    done

--- src/components/custom/Header.jsx
+++ src/components/custom/Header.jsx
@@ -62,35 +62,40 @@ function Header() {
           </div>
 
           {isHomePage && (
-            <>
-              <nav className="hidden lg:flex space-x-6">
-                {["Features", "Pricing", "Testimonials", "Contact"].map(
-                  (item) => (
-                    <button
-                      key={item}
-                      onClick={
-                        item !== "Contact"
-                          ? () => scrollToSection(item.toLowerCase())
-                          : () => goToContact()
-                      }
-                      className="text-black dark:text-white font-semibold dark:hover:text-[#38da97] transition-all duration-300 px-3 py-2 rounded-lg hover:bg-black hover:text-white dark:hover:bg-white/10 transform hover:scale-105">
-                      {item}
-                    </button>
-                  )
-                )}
-              </nav>
-              <button
-                className="lg:hidden text-black dark:text-white"
-                onClick={() => setIsMenuOpen(!isMenuOpen)}>
-                <Menu size={24} />
-              </button>
-            </>
+            <nav className="hidden lg:flex space-x-6">
+              {["Features", "Pricing", "Testimonials", "Contact"].map(
+                (item) => (
+                  <button
+                    key={item}
+                    onClick={
+                      item !== "Contact"
+                        ? () => scrollToSection(item.toLowerCase())
+                        : () => goToContact()
+                    }
+                    className="text-black dark:text-white font-semibold dark:hover:text-[#38da97] transition-all duration-300 px-3 py-2 rounded-lg hover:bg-black hover:text-white dark:hover:bg-white/10 transform hover:scale-105">
+                    {item}
+                  </button>
+                )
+              )}
+            </nav>
           )}
+
+          <SignedIn>
+            <button
+              className={`${
+                isHomePage ? "lg:hidden" : "md:hidden"
+              } text-black dark:text-white`}
+              onClick={() => setIsMenuOpen(!isMenuOpen)}>
+              <Menu size={24} />
+            </button>
+          </SignedIn>
 
           <div className="flex items-center space-x-4">
             <ThemeToggle />
             <SignedIn>
-              <Link to="/create-trip" className="hidden sm:block">
+              <Link
+                to="/create-trip"
+                className={`hidden ${isHomePage ? "lg:block" : "md:block"}`}>
                 <Button
                   className="font-bold bg-gradient-to-tr from-blue-400 via-orange-200 to-blue-400 dark:from-dark-primary dark:via-black dark:to-dark-primary text-black dark:text-white
                   border-2 border-light-border dark:border-dark-border transition-all rounded-xl shadow-md hover:shadow-lg transform hover:scale-105">
@@ -98,7 +103,9 @@ function Header() {
                 </Button>
               </Link>
               <div className="flex items-center space-x-4">
-                <Link to="/my-trips" className="hidden sm:block">
+                <Link
+                  to="/my-trips"
+                  className={`hidden ${isHomePage ? "lg:block" : "md:block"}`}>
                   <Button className="bg-black text-white font-bold hover:bg-gray-800 transition-all rounded-xl shadow-md hover:shadow-lg transform hover:scale-105">
                     View Trips
                   </Button>
@@ -125,41 +132,39 @@ function Header() {
           </div>
         </div>
       </div>
-      {isMenuOpen && isHomePage && (
-        <div className="lg:hidden bg-light-background dark:bg-dark-background py-4 flex flex-col sm:flex-row">
-          {["Features", "Pricing", "Testimonials", "Contact"].map((item) => (
-            <button
-              key={item}
-              onClick={() => scrollToSection(item.toLowerCase())}
-              className="block w-full text-left text-light-foreground dark:text-white font-semibold hover:text-white dark:hover:text-[#38da97] transition-all duration-300 px-6 py-2 sm:text-center
-              dark:hover:bg-white/10 hover:bg-black">
-              {item}
-            </button>
-          ))}
-          <Link to="/create-trip" className="block w-64 px-6 py-2 sm:hidden">
-            <Button
-              className="w-full font-bold bg-gradient-to-tr from-light-primary via-light-primary/50 to-light-secondary dark:from-black dark:via-black/50 dark:to-dark-primary text-white border-2 border-light-border dark:border-dark-border
-              hover:text-white transition-all duration-300 rounded-xl shadow-md hover:shadow-lg transform hover:scale-105">
-              Create new trip
-            </Button>
-          </Link>
-          {isSignedIn ? (
-            <Link to="/my-trips" className="block w-64 px-6 py-2 mt-2 sm:hidden">
+      {isMenuOpen && isSignedIn && (
+        <div
+          className={`${
+            isHomePage ? "lg:hidden" : "md:hidden"
+          } bg-light-background dark:bg-dark-background py-4 flex flex-col`}>
+          {isHomePage &&
+            ["Features", "Pricing", "Testimonials", "Contact"].map((item) => (
+              <button
+                key={item}
+                onClick={
+                  item !== "Contact"
+                    ? () => scrollToSection(item.toLowerCase())
+                    : () => goToContact()
+                }
+                className="block w-full text-left text-light-foreground dark:text-white font-semibold hover:text-white dark:hover:text-[#38da97] transition-all duration-300 px-6 py-2
+                dark:hover:bg-white/10 hover:bg-black">
+                {item}
+              </button>
+            ))}
+          <div className="flex flex-col items-center w-full">
+            <Link to="/create-trip" className="w-64 px-6 py-2">
+              <Button
+                className="w-full font-bold bg-gradient-to-tr from-light-primary via-light-primary/50 to-light-secondary dark:from-black dark:via-black/50 dark:to-dark-primary text-white border-2 border-light-border dark:border-dark-border
+                hover:text-white transition-all duration-300 rounded-xl shadow-md hover:shadow-lg transform hover:scale-105">
+                Create new trip
+              </Button>
+            </Link>
+            <Link to="/my-trips" className="w-64 px-6 py-2">
               <Button className="w-full bg-black text-white font-bold border-2 border-gray-300 hover:bg-gray-800 transition-all duration-300 rounded-xl shadow-md hover:shadow-lg transform hover:scale-105">
                 View Trips
               </Button>
             </Link>
-          ) : (
-            <SignInButton mode="modal">
-              <div className="block w-64 px-6 py-2 mt-2 sm:hidden">
-                <Button
-                  variant="outline"
-                  className="w-full bg-black text-white font-bold hover:bg-gray-800 hover:text-white transition-all duration-300 rounded-xl shadow-md hover:shadow-lg transform hover:scale-105">
-                  Sign In
-                </Button>
-              </div>
-            </SignInButton>
-          )}
+          </div>
         </div>
       )}
     </div>
